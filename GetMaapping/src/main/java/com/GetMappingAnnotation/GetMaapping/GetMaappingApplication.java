ee5b86db864c1d4549d337c49efbb7c57e3fcc6c
vaibhav kharat <vaibhavkharat88@gmail.com>
2024-02-21 14:08:50 +0530

    @GetMapping Annotation

---
+++ GetMaapping/src/main/java/com/GetMappingAnnotation/GetMaapping/GetMaappingApplication.java
@@ -0,0 +1,13 @@
+package com.GetMappingAnnotation.GetMaapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetMaappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetMaappingApplication.class, args);
+	}
+
+}
